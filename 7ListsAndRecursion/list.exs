49dbea7eb1ceea37f06ab04c7662281f050f89e2
intercaetera <marek@intercaetera.com>
2020-06-20 13:04:15 +0200

    Added more list tasks

--- 7ListsAndRecursion/list.exs
+++ 7ListsAndRecursion/list.exs
@@ -33,4 +33,68 @@ defmodule MyList do
   # ListsAndRecursion-4
   def span(from, to) when from >= to, do: [to]
   def span(from, to) when from < to, do: [from | span(from + 1, to)]
+
+  # ListsAndRecursion-5
+  def all?([], _func), do: true
+
+  def all?([head | tail], func) do
+    if func.(head) === true do
+      all?(tail, func)
+    else
+      false
+    end
+  end
+
+  def any?([], _func), do: false
+
+  def any?([head | tail], func) do
+    if func.(head) === true do
+      true
+    else
+      any?(tail, func)
+    end
+  end
+
+  def each([], _func), do: :ok
+
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
+
+  def filter([], _func), do: []
+
+  def filter([head | tail], func) do
+    if func.(head) === true do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  # ListsAndRecursion-8
+  @tax_rates [NC: 0.075, TX: 0.08, CA: 0.05, AK: 0.01]
+
+  @orders [
+    [id: 123, ship_to: :NC, net_amount: 100.00],
+    [id: 124, ship_to: :OK, net_amount: 35.50],
+    [id: 125, ship_to: :TX, net_amount: 24.00],
+    [id: 126, ship_to: :TX, net_amount: 44.80],
+    [id: 127, ship_to: :NC, net_amount: 25.00],
+    [id: 128, ship_to: :MA, net_amount: 10.00],
+    [id: 129, ship_to: :CA, net_amount: 102.00],
+    [id: 130, ship_to: :NC, net_amount: 50.00]
+  ]
+
+  def calculate_total(orders \\ @orders, tax_rates \\ @tax_rates) do
+    for [{:id, id}, {:ship_to, ship_to}, {:net_amount, net_amount}] <- orders do
+      total_amount =
+        case tax_rates[ship_to] do
+          nil -> net_amount
+          _ -> net_amount + net_amount * tax_rates[ship_to]
+        end
+
+      [id: id, ship_to: ship_to, net_amount: net_amount, total_amount: total_amount]
+    end
+  end
 end
